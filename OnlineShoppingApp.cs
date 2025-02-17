36813037831f2ce036dcf4d8aca405067c2701f7
RudTadevosyan <rudtadevosyan07@mail.ru>
2025-02-18 01:31:57 +0400

    Add files via upload

---
+++ OnlineShoppingApp.cs
@@ -0,0 +1,94 @@
+﻿namespace OnlineShoppingApp;
+
+class Program
+{
+    public class Product
+    {
+        public string Name;
+        public float Price;
+        public int Quantity;
+
+        public Product(string name, float price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public float TotalPrice()
+        {
+            return Price * Quantity;
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        int n = 0;
+        Console.Write("How many product you want to buy: ");
+        
+
+        while(!int.TryParse(Console.ReadLine(), out n))
+        {
+            Console.WriteLine("Invalid input!");
+            Console.Write("How many products you want to buy: ");
+        }
+
+        Console.WriteLine();
+        
+        if(n <= 0)
+        {
+            return;
+        }
+
+        Product[] shoppingCart = new Product[n];
+        Random random = new Random();
+
+
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine();
+            string? name;
+            do{
+                Console.Write("Input a product name you want to buy: ");
+                name = Console.ReadLine();
+            }while(string.IsNullOrEmpty(name));
+
+
+            float price;
+            Console.Write("Enter the price: ");
+            while(!float.TryParse(Console.ReadLine(), out price))
+            {
+                Console.WriteLine("Invalid input!");
+                Console.Write("Enter the price: ");
+            }
+
+            int quantity;
+            Console.Write("Enter the quantity: ");
+            while(!int.TryParse(Console.ReadLine(), out quantity))
+            {
+                Console.WriteLine("Invalid input!");
+                Console.Write("Enter the quantity: ");
+            }
+
+            shoppingCart[i] = new Product(name, price, quantity);
+
+        }
+
+        float totalPrice = 0f;
+
+        foreach(Product product in shoppingCart)
+        {
+            totalPrice += product.TotalPrice();
+        }
+
+        if(n > 5)
+        {
+            totalPrice *= 0.9f;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine($"Total price: {totalPrice}");
+
+    }
+}
---
+++ PhoneBookApp.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace PhoneBookApp
+{
+    class Program
+    {
+        public class PhoneBookInfos
+        {
+            public string Name;
+            public string PhoneNumber;
+            public string Email;
+
+            public PhoneBookInfos(string name, string phoneNumber, string email)
+            {
+                Name = name;
+                PhoneNumber = phoneNumber;
+                Email = email;
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {Name}\nPhone number: {PhoneNumber}\nEmail: {Email}\n");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PhoneBookInfos[] phoneBook = new PhoneBookInfos[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Enter details for person {i + 1}:");
+
+                string? name;
+                do
+                {
+                    Console.Write("Name: ");
+                    name = Console.ReadLine();
+                } while (string.IsNullOrWhiteSpace(name));
+
+                string? phoneNumber;
+                do
+                {
+                    Console.Write("Phone number: ");
+                    phoneNumber = Console.ReadLine();
+                } while (string.IsNullOrWhiteSpace(phoneNumber));
+
+                string? email;
+                do
+                {
+                    Console.Write("Email: ");
+                    email = Console.ReadLine();
+                } while (string.IsNullOrWhiteSpace(email));
+
+                phoneBook[i] = new PhoneBookInfos(name, phoneNumber, email);
+                Console.WriteLine();
+            }
+
+            Console.Write("Input a name to search info: ");
+            string? searchName = Console.ReadLine();
+
+            int flagFound = 0;
+
+            for(int i = 0; i < phoneBook.Length; i++)
+            {
+                if(searchName == phoneBook[i].Name)
+                {
+                    phoneBook[i].DisplayInfo();
+                    flagFound = 1;
+                }
+                
+            }
+
+            if(flagFound == 0)
+            {
+                Console.WriteLine("Not found");
+            }
+
+
+        }
+    }
+}
